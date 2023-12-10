e4d4850ea94c4138a55baec69538e252e9fbcb2c
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-10 03:13:21 +0300

    urun_islemleri_bitti

--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/UrunController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/UrunController.cs
@@ -15,13 +15,19 @@ namespace E_ticaret_sitesi.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var urunler = c.Uruns.ToList();
+            var urunler = c.Uruns.Where(x => x.Durum == true).ToList();
+
             return View(urunler);
         }
 
         [HttpGet]
         public ActionResult YeniUrun()
         {
+            //LINQ (Language-Integrated Query) kullanarak bir veritabanından veri çekme işlemini gerçekleştirir. 
+            //Kodun amacı, "c" adlı bir veritabanındaki "Kategoris" adlı tablodan veri alarak bir SelectListItem listesi oluşturmaktır.
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList() select new SelectListItem { Text = x.KategoriAd, Value = x.KategoriID.ToString() }).ToList();
+
+            ViewBag.dgr1 = deger1;
             return View();
         }
 
@@ -39,6 +45,29 @@ namespace E_ticaret_sitesi.Controllers
             deger.Durum = false;
             c.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList() select new SelectListItem { Text = x.KategoriAd, Value = x.KategoriID.ToString() }).ToList();
+            ViewBag.dgr1 = deger1;
+            var urundeger = c.Uruns.Find(id);
+            return View("UrunGetir", urundeger);
+
+        }
+        public ActionResult UrunGuncelle(Urun p)
+        {
+            var urn = c.Uruns.Find(p.Urunid);
+            urn.AlisFiyat = p.AlisFiyat;
+            urn.Durum = p.Durum;
+            urn.Kategoriid = p.Kategoriid;
+            urn.Marka = p.Marka;
+            urn.SatisFiyat = p.SatisFiyat;
+            urn.Stok = p.Stok;
+            urn.UrunAd = p.UrunAd;
+            urn.UrunGorsel = p.UrunGorsel;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }

2d2061b37391534cc8fa4557c02604e5adbc79df
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-10 23:13:44 +0300

    Personel_islemleri

---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/SatisController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
+
+namespace E_ticaret_sitesi.Controllers
+{
+    public class SatisController : Controller
+    {
+        Context c = new Context();
+        // GET: Satis
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+    }
+}

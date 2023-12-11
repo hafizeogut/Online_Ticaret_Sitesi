fcc3200d9a43b1de43ff39e23740ec127e336529
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-11 21:43:03 +0300

    Istatistik_giris_yapildi

---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/istatistikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_ticaret_sitesi.Controllers
+{
+    public class istatistikController : Controller
+    {
+        // GET: istatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

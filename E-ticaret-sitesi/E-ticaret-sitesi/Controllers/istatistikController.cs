629f2331f623bb536c3762eda9a08d4069fcb6ff
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-12 21:45:04 +0300

    Isatistik_sayfa_islemleri

--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/istatistikController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/istatistikController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
 
 namespace E_ticaret_sitesi.Controllers
 {
     public class istatistikController : Controller
     {
         // GET: istatistik
+        Context c = new Context();
+
         public ActionResult Index()
         {
+            var deger1 = c.Carilers.Count().ToString();
+            ViewBag.d1 = deger1;
+
+            var deger2 = c.Uruns.Count().ToString();
+            ViewBag.d2 = deger2;
+
+            var deger3 = c.Personels.Count().ToString();
+            ViewBag.d3 = deger3;
+
+            var deger4 = c.Carilers.Count().ToString();
+            ViewBag.d4 = deger4;
+
+            var deger5 = c.Uruns.Sum(x=>x.Stok).ToString();//Stoklara erişildi
+            ViewBag.d5 = deger5;
+
+            var deger6 = (from x in c.Uruns select x.Marka).Distinct().Count().ToString();
+            ViewBag.d6 = deger6;
+
+
+            var deger7 = c.Uruns.Count(x => x.Stok<=20).ToString(); //Stok 20 den az kalmış ürünler sayıldı.
+            ViewBag.d7 = deger7;
+
+
             return View();
+            
         }
     }
 }

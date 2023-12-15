fd6a0701a51dba5e4fbd70eba5d643e2f87ec04e
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-16 02:43:54 +0300

    Login_islemleri_web_config

---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/CariPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_ticaret_sitesi.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/LoginController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/LoginController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
 
 namespace E_ticaret_sitesi.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult Partial1()
         {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
             return PartialView();
         }
     }

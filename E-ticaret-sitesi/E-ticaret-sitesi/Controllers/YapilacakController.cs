a406ab2f5f4513c220ed3e71855c83c642a4365b
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-15 14:04:41 +0300

    To_do_list_17_bolum

---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/YapilacakController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
+
+
+namespace E_ticaret_sitesi.Controllers
+{
+    public class YapilacakController : Controller
+    {
+        // GET: Yapilacak
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger1 = c.Carilers.Count().ToString();//Carilerin sayısına ulaşıldı
+            ViewBag.d1 = deger1;
+            var deger2 = c.Uruns.Count().ToString();//Toplam Ürün sayısı
+            ViewBag.d2 = deger2;
+            var deger3 = c.Kategoris.Count().ToString();
+            ViewBag.d3 = deger3;
+
+            var deger4 = (from x in c.Carilers select x.CariSehir).Distinct().Count().ToString();
+            ViewBag.d4 = deger4;
+            return View();
+        }
+    }
+}
--- E-ticaret-sitesi/E-ticaret-sitesi/Models/Siniflar/Context.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Models/Siniflar/Context.cs
@@ -18,5 +18,6 @@ namespace E_ticaret_sitesi.Models.Siniflar
         public DbSet<SatisHareket> SatisHarekets { get; set; }
         public DbSet<Urun> Uruns { get; set; }
         public DbSet<Detay> Detays { get; set; }
+        public DbSet<Yapilacak> Yapilacaks { get; set; }
     }
 }

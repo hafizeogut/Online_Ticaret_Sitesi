db4d5ffc210a485e81450854093dda3fdf1abe76
Erdal Özbademci <b200109036@subu.edu.tr>
2023-12-16 00:14:42 +0300

    completed departman

---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/DepartmanController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
+namespace E_ticaret_sitesi.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        Context c = new Context();
+        // GET: Departman
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.Where(x => x.Durum == true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+        {
+            c.Departmans.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DepartmanSil(int id)
+        {
+            var dep = c.Departmans.Find(id);
+            dep.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanGetir(int id)
+        {
+            var dpt = c.Departmans.Find(id);
+            return View("DepartmanGetir", dpt);
+        }
+
+        public ActionResult DepartmanGuncelle(Departman p)
+        {
+            var dept = c.Departmans.Find(p.Departmanid);
+            dept.DepartmanAd = p.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult DepartmanDetay(int id)
+        {
+            var degerler = c.Personels.Where(x => x.Departmanid == id).ToList();
+            var dpt = c.Departmans.Where(x => x.Departmanid == id).Select(y => y.DepartmanAd).FirstOrDefault();
+            ViewBag.d = dpt;
+            return View(degerler);
+        }
+
+        public ActionResult DepartmanPersonelSatis(int id)
+        {
+            var degerler = c.SatisHarekets.Where(x => x.Personelid == id).ToList();
+            var per = c.Personels.Where(x => x.Personelid == id).Select(y => y.PersonelAd + " " + y.PersonelSoyad).FirstOrDefault();
+            ViewBag.dpers = per;
+            return View(degerler);
+        }
+
+
+
+    }
+}

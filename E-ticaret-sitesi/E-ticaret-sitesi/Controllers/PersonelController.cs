a80883b7e3c4a7d8f3702a2c342b5243c785acd8
Erdal Özbademci <b200109036@subu.edu.tr>
2023-12-16 01:28:29 +0300

    completed personel

---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/PersonelController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
+
+
+namespace E_ticaret_sitesi.Controllers
+{
+    public class PersonelController : Controller
+    {
+        Context c = new Context();
+        // GET: Personel
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+        [HttpGet]//form yüklendipinde bu metodu çalıstır
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList() select new SelectListItem { Text = x.DepartmanAd, Value = x.Departmanid.ToString() }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+
+        [HttpPost]//Bir butona tıklandığı zaman bu metodu çalıstır
+        public ActionResult PersonelEkle(Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList() select new SelectListItem { Text = x.DepartmanAd, Value = x.Departmanid.ToString() }).ToList();
+            ViewBag.dgr1 = deger1;
+            var prs = c.Personels.Find(id);
+            return View("PersonelGetir", prs);
+        }
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prsn = c.Personels.Find(p.Personelid);
+            prsn.PersonelAd = p.PersonelAd;
+            prsn.PersonelSoyad = p.PersonelSoyad;
+            prsn.PersonelGorsel = p.PersonelGorsel;
+            prsn.Departmanid = p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}

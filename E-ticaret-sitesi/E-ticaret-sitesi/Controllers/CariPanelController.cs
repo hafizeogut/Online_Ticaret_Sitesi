e84d46e25e98844e9c361bfd6b0c57a8c4dd61ae
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-16 03:45:16 +0300

    Login_islenleri_mail_sifre_karsilastirma

--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/CariPanelController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/CariPanelController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_ticaret_sitesi.Models.Siniflar;
 
 namespace E_ticaret_sitesi.Controllers
 {
     public class CariPanelController : Controller
     {
         // GET: CariPanel
+        
+        Context c = new Context();
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var mail = (string)Session["CariMail"];//Veri tabanındaki mail adresine ulaşıldı
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
         }
     }
 }

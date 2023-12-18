89434de447dd7f3ed9b0bf428865bda3606c4490
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-19 01:31:37 +0300

    Mesajlar_bolumu_bitti

--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/CariPanelController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/CariPanelController.cs
@@ -33,7 +33,7 @@ namespace E_ticaret_sitesi.Controllers
         public ActionResult GelenMesajlar()
         {
             var mail = (string)Session["CariMail"];
-            var mesajlar = c.Mesajlars.Where(x=>x.Alıcı== mail).ToList();
+            var mesajlar = c.Mesajlars.Where(x=>x.Alıcı== mail).OrderByDescending(x=> x.MesajID).ToList();
             var gelensayisi = c.Mesajlars.Count(x => x.Alıcı == mail).ToString();
             ViewBag.d1 = gelensayisi;
             var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
@@ -43,7 +43,7 @@ namespace E_ticaret_sitesi.Controllers
         public ActionResult GidenMesajlar()
         {
             var mail = (string)Session["CariMail"];
-            var mesajlar = c.Mesajlars.Where(x => x.Gonderici == mail).ToList();
+            var mesajlar = c.Mesajlars.Where(x => x.Gonderici == mail).OrderByDescending(z => z.MesajID).ToList();
             var gelensayisi = c.Mesajlars.Count(x => x.Alıcı == mail).ToString();
             ViewBag.d1 = gelensayisi;
             var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
@@ -66,12 +66,26 @@ namespace E_ticaret_sitesi.Controllers
         [HttpGet]
         public ActionResult YeniMesaj()
         {
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.Mesajlars.Where(x => x.Alıcı == mail).ToList();
+
+
+            var gelensayisi = c.Mesajlars.Count(x => x.Alıcı == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
             return View();
 
         }
         [HttpPost]
         public ActionResult YeniMesaj(Mesajlar m)
         {
+            var mail = (string)Session["CariMail"];
+            m.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            m.Gonderici = mail;
+
+            c.Mesajlars.Add(m);
+            c.SaveChanges();
             return View();
 
         }

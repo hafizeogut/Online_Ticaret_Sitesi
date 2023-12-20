a57eecc04c5721ddedce15068abe0ccb58163449
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-21 02:59:25 +0300

    Fatura_detay

--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/FaturaController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/FaturaController.cs
@@ -64,5 +64,32 @@ namespace E_ticaret_sitesi.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Dinamik()
+        {
+            //bir sayfada birden fazla tablodan veri çekme işlemleri oıluşturuluyor
+            Class4 cs = new Class4();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+            return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSıraNo,DateTime Tarih,
+            string VergiDairesi,string Saat,string TeslimEden,string TeslimAlan,string Toplam,FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSıraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            
+            f.VergiDairesi = VergiDairesi;
+
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam =decimal.Parse( Toplam );
+            c.Faturalars.Add(f);
+            c.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+
+        }
     }
 }
---
+++ E-ticaret-sitesi/E-ticaret-sitesi/Models/Siniflar/Class4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_ticaret_sitesi.Models.Siniflar
+{
+    public class Class4
+    {
+        public IEnumerable<Faturalar> deger1 { set; get; }
+        public IEnumerable<FaturaKalem> deger2 { set; get; }
+    }
+}

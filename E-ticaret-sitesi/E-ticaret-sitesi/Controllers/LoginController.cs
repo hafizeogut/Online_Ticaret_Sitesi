a59d4b1466acd21f9faf86691c0c8fa9c0577df8
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-12-26 05:59:20 +0300

    finish_project

--- E-ticaret-sitesi/E-ticaret-sitesi/Controllers/LoginController.cs
+++ E-ticaret-sitesi/E-ticaret-sitesi/Controllers/LoginController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using E_ticaret_sitesi.Models.Siniflar;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using E_ticaret_sitesi.Models.Siniflar;
 
 namespace E_ticaret_sitesi.Controllers
 {
